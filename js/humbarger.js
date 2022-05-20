cfad5afe1973f15e3897cc8024f8a6584ef636ae
sskhkr1993 <krkkou65@gmail.com>
2022-05-20 17:09:42 +0900

    menuの修正とlayout、objectの中身の修正

--- js/humbarger.js
+++ js/humbarger.js
@@ -1,6 +1,6 @@
 $(function (){
   $(".js-button").on("click",function(){
-    $(".l-menu,.menu-button__close,.menu-background").toggleClass("is-open");
+    $(".menu-wrapper,.menu-button__close,.menu-background").toggleClass("is-open");
     $("body").toggleClass("is-open"); /*bodyがスクロールできなくなる用*/
   });
 });
